b66a3cf865144b6e1c23c3db51e08b7b7e086681
LovelyDev829 <lovelydeveloper829@gmail.com>
2023-01-20 05:10:23 +0900

    responsive version 1

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,12 +8,12 @@ import { ReactComponent as MenuIcon } from "../assets/svg/menu.svg";
 import { ReactComponent as LinkIcon } from "../assets/svg/link.svg";
 // import { ReactComponent as CameraIcon } from "../assets/svg/camera.svg";
 import { ReactComponent as DownloadIcon } from "../assets/svg/download.svg";
-import { ReactComponent as BookmarkIcon } from "../assets/svg/bookmark.svg";
+// import { ReactComponent as BookmarkIcon } from "../assets/svg/bookmark.svg";
 import { ReactComponent as FieldIcon } from "../assets/svg/field.svg";
 import { ReactComponent as MinimizeIcon } from "../assets/svg/minimize.svg";
 import { ReactComponent as MaximizeIcon } from "../assets/svg/maximize.svg";
 import { ReactComponent as VideoIcon } from "../assets/svg/video.svg";
-import { ReactComponent as UsersIcon } from "../assets/svg/users.svg";
+// import { ReactComponent as UsersIcon } from "../assets/svg/users.svg";
 
 import { ReactComponent as GlobeIcon } from "../assets/svg/globe.svg";
 import { ReactComponent as HelpCircleIcon } from "../assets/svg/help-circle.svg";
@@ -41,6 +41,13 @@ function MainPage({
   fullScreenHandle
 }) {
   const navigate = useNavigate();
+  // const [landFlag, setLandFlag] = useState(false)
+  // useEffect(()=>{
+  //   const height = window.innerHeight
+  //   const width = window.innerWidth
+  //   if(height > width) setLandFlag(false)
+  //   else setLandFlag(true)
+  // }, [landFlag])
   useEffect(() => {
     if (!document.mozFullScreen && !document.webkitIsFullScreen)
       setFullScreenFlag(false);
@@ -66,163 +73,172 @@ function MainPage({
   };
   return (
     <div className="MainPage">
-      <div className="board">
-        <div className="button-line">
-          <div className="button-group">
-            <div className="button">
-              <MenuIcon />
-            </div>
-            <div className="button">
-              <LinkIcon />
-            </div>
-            <div
-              className="button"
-              onClick={() =>
-                exportAsImage(
-                  document.getElementById("image-to-download"),
-                  "basketball-screen",
-                  true
-                )}
-            >
-              <DownloadIcon />
-            </div>
-            <div className="button">
-              <BookmarkIcon />
-            </div>
-            <div
-              className={fieldLineFlag ? "button" : "button clicked"}
-              onClick={() => {
-                setFieldLineFlag(!fieldLineFlag);
-              }}
-            >
-              <FieldIcon />
-            </div>
-            <div
-              className={!fullScreenFlag ? "button" : "button clicked"}
-              onClick={() => {
-                if (fullScreenFlag) fullScreenHandle.exit();
-                else fullScreenHandle.enter();
-                setFullScreenFlag(!fullScreenFlag);
-              }}
-            >
-              {!fullScreenFlag ? <MaximizeIcon /> : <MinimizeIcon />}
-            </div>
-            <div className="button" onClick={() => navigate("/animation")}>
-              <VideoIcon style={{ color: "red", stroke: "red" }} />
-            </div>
-            <div className="button">
-              <UsersIcon />
-            </div>
-          </div>
-          <div className="button-group">
-            <div className="button">
-              <GlobeIcon />
-            </div>
-            <div className="button">
-              <HelpCircleIcon />
-            </div>
-            <div className="button">
-              <LogInIcon />
-            </div>
-          </div>
-        </div>
-        <div id="image-to-download" className="image-to-download">
-          <img
-            src={fieldLineFlag ? fieldLine : fieldWithoutLine}
-            alt="BACKGROUND"
-          />
-        </div>
-        <div className="button-line">
-          <div className="circles">
-            <div className="circle red">1</div>
-            <div className="circle blue">1</div>
-            <div className="circle brown">1</div>
-            <div className="circle yellow">1</div>
-            <div className="circle green">1</div>
-            <div className="circle white">1</div>
-            <div className="circle grey">1</div>
-            <div className="circle black">1</div>
-
-            <div className="point purple" />
-            <div className="point orange" />
-            <div className="point springgreen" />
-            <div className="point cornflowerblue" />
-            <div className="ball">
-              <BallIcon />
-            </div>
-          </div>
-          <div className="button-group">
-            <div className="button">
-              <RotateIcon />
-            </div>
-            <div className="button">
-              <TrashIcon />
-            </div>
-          </div>
+      <div className="top-user-info">
+        <img src={mainLogo} alt="" />
+        <div className="user-avatar">
+          <UserIcon />
+          Williams
         </div>
       </div>
-      <div className="roster">
-        <div className="user-info">
-          <img src={mainLogo} alt="" />
-          <div className="user-avatar">
-            <UserIcon />
-            Williams
+      <div className="main">
+        <div className="board">
+          <div className="button-line">
+            <div className="button-group">
+              <div className="button">
+                <MenuIcon />
+              </div>
+              <div className="button">
+                <LinkIcon />
+              </div>
+              <div
+                className="button"
+                onClick={() =>
+                  exportAsImage(
+                    document.getElementById("image-to-download"),
+                    "basketball-screen",
+                    true
+                  )}
+              >
+                <DownloadIcon />
+              </div>
+              {/* <div className="button">
+              <BookmarkIcon />
+            </div> */}
+              <div
+                className={fieldLineFlag ? "button" : "button clicked"}
+                onClick={() => {
+                  setFieldLineFlag(!fieldLineFlag);
+                }}
+              >
+                <FieldIcon />
+              </div>
+              <div
+                className={!fullScreenFlag ? "button" : "button clicked"}
+                onClick={() => {
+                  if (fullScreenFlag) fullScreenHandle.exit();
+                  else fullScreenHandle.enter();
+                  setFullScreenFlag(!fullScreenFlag);
+                }}
+              >
+                {!fullScreenFlag ? <MaximizeIcon /> : <MinimizeIcon />}
+              </div>
+              <div className="button" onClick={() => navigate("/animation")}>
+                <VideoIcon style={{ color: "red", stroke: "red" }} />
+              </div>
+              {/* <div className="button">
+              <UsersIcon />
+            </div> */}
+            </div>
+            <div className="button-group">
+              <div className="button">
+                <GlobeIcon />
+              </div>
+              <div className="button">
+                <HelpCircleIcon />
+              </div>
+              <div className="button">
+                <LogInIcon />
+              </div>
+            </div>
+          </div>
+          <div id="image-to-download" className="image-to-download">
+            <img
+              src={fieldLineFlag ? fieldLine : fieldWithoutLine}
+              alt="BACKGROUND"
+            />
+          </div>
+          <div className="button-line">
+            <div className="circles">
+              <div className="circle red">1</div>
+              <div className="circle blue">1</div>
+              <div className="circle brown">1</div>
+              <div className="circle yellow">1</div>
+              <div className="circle green">1</div>
+              <div className="circle white">1</div>
+              <div className="circle grey">1</div>
+              <div className="circle black">1</div>
+
+              <div className="point purple" />
+              <div className="point orange" />
+              <div className="point springgreen" />
+              <div className="point cornflowerblue" />
+              <div className="ball">
+                <BallIcon />
+              </div>
+            </div>
+            <div className="button-group">
+              <div className="button">
+                <RotateIcon />
+              </div>
+              <div className="button">
+                <TrashIcon />
+              </div>
+            </div>
           </div>
         </div>
-        <div className="table">
-          <div className="top">
-            <div className="left">
-              <div className="title">Offense</div>
-              <div className="list">
-                <div className="member">23 M.Jordan</div>
-                <div className="member">23 M.Jordan</div>
-                <div className="member">23 M.Jordan</div>
-                <div className="member">23 M.Jordan</div>
-                <div className="member">23 M.Jordan</div>
-              </div>
-            </div>
-            <div className="right">
-              <div className="title">Defense</div>
-              <div className="list">
-                <div className="plan">Man-to-Man</div>
-                <div className="plan">2-3 Zone</div>
-                <div className="plan">1-3-1 Zone</div>
-                <div className="plan">3-2 Zone</div>
-                <div className="plan">Custom</div>
-              </div>
+        <div className="roster">
+          <div className="hidden">
+            <img src={mainLogo} alt="" />
+            <div className="user-avatar">
+              <UserIcon />
+              Williams
             </div>
           </div>
-          <div className="bottom">
-            <div className="title">Roster</div>
-            <div className="columns">
-              <div className="column">
-                <div className="sub-title">GUARDS</div>
+          <div className="table">
+            <div className="top">
+              <div className="left">
+                <div className="title">Offense</div>
                 <div className="list">
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
+                  <div className="member">23 M.Jordan</div>
+                  <div className="member">23 M.Jordan</div>
+                  <div className="member">23 M.Jordan</div>
+                  <div className="member">23 M.Jordan</div>
+                  <div className="member">23 M.Jordan</div>
                 </div>
               </div>
-              <div className="column">
-                <div className="sub-title">GUARDS</div>
+              <div className="right">
+                <div className="title">Defense</div>
                 <div className="list">
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
+                  <div className="plan">Man-to-Man</div>
+                  <div className="plan">2-3 Zone</div>
+                  <div className="plan">1-3-1 Zone</div>
+                  <div className="plan">3-2 Zone</div>
+                  <div className="plan">Custom</div>
                 </div>
               </div>
-              <div className="column">
-                <div className="sub-title">GUARDS</div>
-                <div className="list">
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
-                  <div className="item">23 M.Jordan</div>
+            </div>
+            <div className="bottom">
+              <div className="title">Roster</div>
+              <div className="columns">
+                <div className="column">
+                  <div className="sub-title">GUARDS</div>
+                  <div className="list">
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                  </div>
+                </div>
+                <div className="column">
+                  <div className="sub-title">GUARDS</div>
+                  <div className="list">
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                  </div>
+                </div>
+                <div className="column">
+                  <div className="sub-title">GUARDS</div>
+                  <div className="list">
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                    <div className="item">23 M.Jordan</div>
+                  </div>
                 </div>
               </div>
             </div>
